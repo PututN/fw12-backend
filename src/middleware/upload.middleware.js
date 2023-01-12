6b6c50d06e55157ac5ba8bef74e4520238008494
Putut Nardianto L <pututnardiantol@gmail.com>
2023-01-12 22:08:54 +0700

    fix upload

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,68 +1,43 @@
 const multer = require("multer");
-const fs = require("fs");
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const path = require("path");
+const errorHandler = require("../helpers/errorHandler.helper");
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     const dirPath = "uploads/";
-//     if (!fs.existsSync(dirPath)) {
-//       fs.mkdirSync(dirPath);
-//     }
-//     callback(null, dirPath);
-//   },
-//   filename: (req, file, callback) => {
-//     const getExtension = file.originalname.split(".");
-//     const extension = getExtension[getExtension.length - 1];
-//     const name = `${new Date().getDate()}_${new Date().getTime()}.${extension}`;
-//     callback(null, name);
-//   },
-// });
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "cinemnar",
-    format: async (req, file) => path.extname(file.originalname).slice("1"), // supports promises as well
-    public_id: (req, file) => {
-      const randomNumber = Math.round(Math.random() * 90000);
-      const name = `${new Date().getDate()}_${randomNumber}`;
-      return name;
-    },
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const extention = file.originalname.split(".");
+    const ext = extention[extention.length - 1];
+    const name = `${new Date().getDate()}_${new Date().getTime()}.${ext}`;
+    cb(null, name);
   },
 });
+
+const limit = 1024 * 1024;
 
 const upload = multer({
-  storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: (req, file, callback) => {
-    const format = ["jpg", "png", "jpeg"];
-    const extension = file.originalname.split(".");
-    const cekFormatFile = format.includes(extension[extension.length - 1]);
-    if (!cekFormatFile) {
-      return callback(new Error("Format picture not valid"));
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg"
+    ) {
+      cb(null, true);
     } else {
-      return callback(null, true);
+      cb(new Error("Only png,jpg and jpeg format allowed"), false);
     }
   },
+  limits: { fileSize: limit },
 });
 
-const uploadFile = upload.single("picture");
-const uploadMiddleware = (req, res, next) => {
-  uploadFile(req, res, (error) => {
-    if (error) {
-      return res.status(400).json({
-        success: false,
-        message: error.message,
-      });
+const uploadMiddleware = upload.single("picture");
+
+module.exports = (req, res, next) => {
+  uploadMiddleware(req, res, (err) => {
+    if (err) {
+      return errorHandler(err, res);
     }
     next();
   });
 };
-module.exports = { uploadMiddleware, cloudinary };
