357f0b2e391413b3d721c1733d1956f5c29d9269
Putut Nardianto L <pututnardiantol@gmail.com>
2023-01-16 19:31:26 +0700

    add moviescheduleid on movie detail

--- src/models/movieDetail.model.js
+++ src/models/movieDetail.model.js
@@ -20,14 +20,14 @@ exports.getScheduleByCityModel = async (id, date, cb) => {
 
 exports.getScheduleByMovieModel = async (id, city, date, cb) => {
   try {
-    const sql = `SELECT c.id, c.picture, c.name, c.address, c.city, mS.price, string_to_array(string_agg(DISTINCT mST.time::VARCHAR, ', '), ', ') as time
+    const sql = `SELECT mS.id as movieScheduleId, c.id, c.picture, c.name, c.address, c.city, mS.price, string_to_array(string_agg(DISTINCT mST.time::VARCHAR, ', '), ', ') as time
     FROM "movieSchedules" mS
     JOIN cinemas c ON mS."cinemaId" = c.id
     JOIN movies m ON mS."movieId" = m.id
     JOIN "movieScheduleTimes" mST ON mS.id = mST."movieScheduleId"
     WHERE m.id = $1 AND c.city = $2
     AND (COALESCE(NULLIF($3, '')::DATE, CURRENT_DATE) BETWEEN mS."startDate" AND mS."endDate")
-    GROUP BY c.id, mS.price`;
+    GROUP BY c.id, mS.price, mS.id`;
     const values = [id, city, date];
     db.query(sql, values, cb);
   } catch (err) {
